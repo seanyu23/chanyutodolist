3bae2eb9cec8b5458db99fbc0836138da6528e97
wesley <wesley_316@yahoo.com.ph>
2014-03-22 01:06:37 +0800

    remove #secret

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,7 @@ ToDoList50::Application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
   config.action_mailer.default_url_options = { host: 'hidden-thicket-3035.herokuapp.com' }
+  
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
